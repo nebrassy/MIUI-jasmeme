4b891c34ea8874835da12b8159251e3e7026ae5a
nebrassy <nebras30@gmail.com>
2020-03-02 12:52:34 +0200

    refactor all

--- main.sh
+++ main.sh
@@ -1,40 +1,36 @@
-# Requires: android tools, img tools, abootimg, cpio, sdat2img, brotli attr
 
 SVENDOR=/mnt/vendora2
 SSYSTEM=/mnt/systema2
-PVENDOR=/mnt/vendorn7
-PSYSTEM=/mnt/systemn7
-CURRENTUSER=nebras30
-SOURCEROM=/home/$CURRENTUSER/aicp10
-SD2IMG=/home/$CURRENTUSER/dev/sdat2img.py
+PVENDOR=/mnt/vendorport
+PSYSTEM=/mnt/systemport
+CURRENTUSER=$4
+SOURCEROM=$3
 SCRIPTDIR=$(readlink -f "$0")
 CURRENTDIR=$(dirname "$SCRIPTDIR")
 FILES=$CURRENTDIR/files
-PORTZIP=/home/$CURRENTUSER/dev/xiaomi.eu*
-STOCKZIP=/home/$CURRENTUSER/dev/jasmine*
+PORTZIP=$1
+STOCKTAR=$2
 OUTP=$CURRENTDIR/out
+TOOLS=$CURRENTDIR/tools
 
 mkdir $OUTP
 cp -Raf $CURRENTDIR/zip $OUTP/
 
-unzip $PORTZIP system.transfer.list vendor.transfer.list system.new.dat.br vendor.new.dat.br
-tar --wildcards -xf $STOCKZIP */images/vendor.img */images/system.img
-mv jasmine_global_images*/images/vendor.img vendor.img
-mv jasmine_global_images*/images/system.img system.img
+unzip -d $OUTP $PORTZIP system.transfer.list vendor.transfer.list system.new.dat.br vendor.new.dat.br
+tar --wildcards -xf $STOCKTAR */images/vendor.img */images/system.img
+mv jasmine_global_images*/images/vendor.img $OUTP/vendor.img
+mv jasmine_global_images*/images/system.img $OUTP/system.img
 rm -rf jasmine_global_images*
  
  
-simg2img system.img systema2.img
-simg2img vendor.img vendora2.img
+simg2img $OUTP/system.img $OUTP/systema2.img
+simg2img $OUTP/vendor.img $OUTP/vendora2.img
 
-#brotli --verbose --decompress --input system.new.dat.br --output system.new.dat
-brotli -j -v -d system.new.dat.br
-#brotli --verbose --decompress --input vendor.new.dat.br --output vendor.new.dat
-brotli -j -v -d vendor.new.dat.br
-$SD2IMG system.transfer.list system.new.dat systemn7.img
-$SD2IMG vendor.transfer.list vendor.new.dat vendorn7.img
-rm system.new.dat.br vendor.new.dat.br vendor.img system.img system.new.dat vendor.new.dat system.transfer.list vendor.transfer.list
-
+brotli -j -v -d $OUTP/system.new.dat.br -o $OUTP/system.new.dat
+brotli -j -v -d $OUTP/vendor.new.dat.br -o $OUTP/vendor.new.dat
+$TOOLS/sdat2img/sdat2img.py $OUTP/system.transfer.list $OUTP/system.new.dat $OUTP/systemport.img
+$TOOLS/sdat2img/sdat2img.py $OUTP/vendor.transfer.list $OUTP/vendor.new.dat $OUTP/vendorport.img
+rm $OUTP/system.new.dat.br $OUTP/vendor.new.dat.br $OUTP/vendor.img $OUTP/system.img $OUTP/system.new.dat $OUTP/vendor.new.dat $OUTP/system.transfer.list $OUTP/vendor.transfer.list
 
 
 unalias cp
@@ -42,29 +38,36 @@ mkdir $PSYSTEM
 mkdir $PVENDOR
 mkdir $SVENDOR
 mkdir $SSYSTEM
-mount -o rw,noatime systemn7.img $PSYSTEM
-mount -o rw,noatime vendorn7.img $PVENDOR
-mount -o rw,noatime systema2.img $SSYSTEM
-mount -o rw,noatime vendora2.img $SVENDOR
+mount -o rw,noatime $OUTP/systemport.img $PSYSTEM
+mount -o rw,noatime $OUTP/vendorport.img $PVENDOR
+mount -o rw,noatime $OUTP/systema2.img $SSYSTEM
+mount -o rw,noatime $OUTP/vendora2.img $SVENDOR
 
 
 #BUILD BOOT IMAGE
-source $CURRENTDIR/buildbootimage.sh
+PATCHDATE=$(sudo grep ro.build.version.security_patch= $PSYSTEM/system/build.prop | sed "s/ro.build.version.security_patch=//g"; )
+if [[ -z $PATCHDATE ]]
+then
+echo "failed to find security patch date, aborting" && exit
+fi
+su -c "$CURRENTDIR/buildbootimage.sh $PATCHDATE $SOURCEROM $OUTP $CURRENTDIR" $CURRENTUSER
 
 rm -rf $PSYSTEM/cache
-cp -Rafv $SSYSTEM/cache $PSYSTEM/
+cp -af $SSYSTEM/cache $PSYSTEM/
 
+mkdir $PSYSTEM/system/addon.d
+setfattr -h -n security.selinux -v u:object_r:system_file:s0 $PSYSTEM/system/addon.d
+chmod 755 $PSYSTEM/system/addon.d
 
-
-cp -Rafv $FILES/bootctl $PSYSTEM/system/bin/
+cp -f $FILES/bootctl $PSYSTEM/system/bin/
 chmod 755 $PSYSTEM/system/bin/bootctl
 setfattr -h -n security.selinux -v u:object_r:system_file:s0 $PSYSTEM/system/bin/bootctl
 
-cp -Raf $SSYSTEM/system/lib/vndk-29/android.hardware.boot@1.0.so $PSYSTEM/system/lib/vndk-29/android.hardware.boot@1.0.so
-cp -Raf $SSYSTEM/system/lib64/vndk-29/android.hardware.boot@1.0.so $PSYSTEM/system/lib64/vndk-29/android.hardware.boot@1.0.so
-cp -Raf $SSYSTEM/system/lib64/android.hardware.boot@1.0.so $PSYSTEM/system/lib64/android.hardware.boot@1.0.so
+cp -af $SSYSTEM/system/lib/vndk-29/android.hardware.boot@1.0.so $PSYSTEM/system/lib/vndk-29/android.hardware.boot@1.0.so
+cp -af $SSYSTEM/system/lib64/vndk-29/android.hardware.boot@1.0.so $PSYSTEM/system/lib64/vndk-29/android.hardware.boot@1.0.so
+cp -af $SSYSTEM/system/lib64/android.hardware.boot@1.0.so $PSYSTEM/system/lib64/android.hardware.boot@1.0.so
 
-cp -Raf $SVENDOR/etc/MIUI_DualCamera_watermark.png $PVENDOR/etc/MIUI_DualCamera_watermark.png
+cp -af $SVENDOR/etc/MIUI_DualCamera_watermark.png $PVENDOR/etc/MIUI_DualCamera_watermark.png
 
 rm -rf $PSYSTEM/system/priv-app/Updater
 
@@ -111,18 +114,18 @@ cp -Raf $SVENDOR/firmware $PVENDOR/firmware
 
 
 #VENDOR
-cp -Rafv $FILES/fstab.qcom $PVENDOR/etc/
+cp -f $FILES/fstab.qcom $PVENDOR/etc/
 chmod 644 $PVENDOR/etc/fstab.qcom
 setfattr -h -n security.selinux -v u:object_r:vendor_configs_file:s0 $PVENDOR/etc/fstab.qcom
 chown -hR root:root $PVENDOR/etc/fstab.qcom
 
 
-cp -Rafv $SVENDOR/bin/hw/android.hardware.boot@1.0-service $PVENDOR/bin/hw/android.hardware.boot@1.0-service
-cp -Rafv $SVENDOR/etc/init/android.hardware.boot@1.0-service.rc $PVENDOR/etc/init/android.hardware.boot@1.0-service.rc
-cp -Rafv $SVENDOR/lib/hw/bootctrl.sdm660.so $PVENDOR/lib/hw/bootctrl.sdm660.so
-cp -Rafv $SVENDOR/lib/hw/android.hardware.boot@1.0-impl.so $PVENDOR/lib/hw/android.hardware.boot@1.0-impl.so
-cp -Rafv $SVENDOR/lib64/hw/bootctrl.sdm660.so $PVENDOR/lib64/hw/bootctrl.sdm660.so
-cp -Rafv $SVENDOR/lib64/hw/android.hardware.boot@1.0-impl.so $PVENDOR/lib64/hw/android.hardware.boot@1.0-impl.so
+cp -af $SVENDOR/bin/hw/android.hardware.boot@1.0-service $PVENDOR/bin/hw/android.hardware.boot@1.0-service
+cp -af $SVENDOR/etc/init/android.hardware.boot@1.0-service.rc $PVENDOR/etc/init/android.hardware.boot@1.0-service.rc
+cp -af $SVENDOR/lib/hw/bootctrl.sdm660.so $PVENDOR/lib/hw/bootctrl.sdm660.so
+cp -af $SVENDOR/lib/hw/android.hardware.boot@1.0-impl.so $PVENDOR/lib/hw/android.hardware.boot@1.0-impl.so
+cp -af $SVENDOR/lib64/hw/bootctrl.sdm660.so $PVENDOR/lib64/hw/bootctrl.sdm660.so
+cp -af $SVENDOR/lib64/hw/android.hardware.boot@1.0-impl.so $PVENDOR/lib64/hw/android.hardware.boot@1.0-impl.so
 
 
 sed -i "58 i \    <hal format=\"hidl\">
@@ -146,57 +149,57 @@ s/4.0::IKeymasterDevice/3.0::IKeymasterDevice/g" $PVENDOR/etc/vintf/manifest.xml
 
 rm -rf $PVENDOR/etc/sensors
 cp -Raf $SVENDOR/etc/sensors $PVENDOR/etc/sensors
-cp -Raf $SVENDOR/etc/camera/camera_config.xml $PVENDOR/etc/camera/camera_config.xml
-cp -Raf $SVENDOR/etc/camera/csidtg_camera.xml $PVENDOR/etc/camera/csidtg_camera.xml
-cp -Raf $SVENDOR/etc/camera/csidtg_chromatix.xml $PVENDOR/etc/camera/camera_chromatix.xml
+cp -af $SVENDOR/etc/camera/camera_config.xml $PVENDOR/etc/camera/camera_config.xml
+cp -af $SVENDOR/etc/camera/csidtg_camera.xml $PVENDOR/etc/camera/csidtg_camera.xml
+cp -af $SVENDOR/etc/camera/csidtg_chromatix.xml $PVENDOR/etc/camera/camera_chromatix.xml
 
-cp -Raf $SVENDOR/lib/libMiWatermark.so $PVENDOR/lib/libMiWatermark.so
-cp -Raf $SVENDOR/lib/libdng_sdk.so $PVENDOR/lib/libdng_sdk.so
-cp -Raf $SVENDOR/lib/libvidhance_gyro.so $PVENDOR/lib/libvidhance_gyro.so
-cp -Raf $SVENDOR/lib/libvidhance.so $PVENDOR/lib/
+cp -af $SVENDOR/lib/libMiWatermark.so $PVENDOR/lib/libMiWatermark.so
+cp -af $SVENDOR/lib/libdng_sdk.so $PVENDOR/lib/libdng_sdk.so
+cp -af $SVENDOR/lib/libvidhance_gyro.so $PVENDOR/lib/libvidhance_gyro.so
+cp -af $SVENDOR/lib/libvidhance.so $PVENDOR/lib/
 
 
-cp -Rafv $FILES/camera/lib/libmmcamera* $PVENDOR/lib/
+cp -f $FILES/camera/lib/libmmcamera* $PVENDOR/lib/
 chmod 644 $PVENDOR/lib/libmmcamera*
 chown -hR root:root $PVENDOR/lib/libmmcamera*
 setfattr -h -n security.selinux -v u:object_r:vendor_file:s0 $PVENDOR/lib/libmmcamera*
 
-cp -Rafv $FILES/camera/lib64/libmmcamera* $PVENDOR/lib64/
+cp -f $FILES/camera/lib64/libmmcamera* $PVENDOR/lib64/
 chmod 644 $PVENDOR/lib64/libmmcamera*
 chown -hR root:root $PVENDOR/lib64/libmmcamera*
 setfattr -h -n security.selinux -v u:object_r:vendor_file:s0 $PVENDOR/lib64/libmmcamera*
 
-cp -Rafv $FILES/camera/lib/hw/camera.sdm660.so $PVENDOR/lib/hw/
+cp -f $FILES/camera/lib/hw/camera.sdm660.so $PVENDOR/lib/hw/
 chmod 644 $PVENDOR/lib/hw/camera.sdm660.so
 chown -hR root:root $PVENDOR/lib/hw/camera.sdm660.so
 setfattr -h -n security.selinux -v u:object_r:vendor_file:s0 $PVENDOR/lib/hw/camera.sdm660.so
 
-cp -Rafv $FILES/camera/lib/camera.sdm660_shim.so $PVENDOR/lib/
+cp -f $FILES/camera/lib/camera.sdm660_shim.so $PVENDOR/lib/
 chmod 644 $PVENDOR/lib/camera.sdm660_shim.so
 chown root:root $PVENDOR/lib/camera.sdm660_shim.so
 setfattr -h -n security.selinux -v u:object_r:vendor_file:s0 $PVENDOR/lib/camera.sdm660_shim.so
 
 #BOOTANIMATION
-cp -Raf $FILES/bootanimation.zip $PSYSTEM/system/media/bootanimation.zip
+cp -f $FILES/bootanimation.zip $PSYSTEM/system/media/bootanimation.zip
 chmod 644 $PSYSTEM/system/media/bootanimation.zip
 chown root:root $PSYSTEM/system/media/bootanimation.zip
 setfattr -h -n security.selinux -v u:object_r:system_file:s0 $PSYSTEM/system/media/bootanimation.zip
 
-cp -Raf $SVENDOR/app/FingerprintExtensionService/FingerprintExtensionService.apk $PVENDOR/app/FingerprintExtensionService/FingerprintExtensionService.apk
-cp -Raf $SVENDOR/framework/com.fingerprints.extension.jar $PVENDOR/framework/com.fingerprints.extension.jar
-cp -Raf $SVENDOR/lib64/hw/fingerprint.fpc.default.so $PVENDOR/lib64/hw/fingerprint.fpc.default.so
-cp -Raf $SVENDOR/lib64/hw/fingerprint.goodix.default.so $PVENDOR/lib64/hw/fingerprint.goodix.default.so
-cp -Raf $SVENDOR/lib64/vendor.qti.hardware.fingerprint@1.0.so $PVENDOR/lib64/vendor.qti.hardware.fingerprint@1.0.so
-cp -Raf $SVENDOR/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so $PVENDOR/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so
-cp -Raf $SVENDOR/lib64/libvendor.goodix.hardware.fingerprint@1.0.so $PVENDOR/lib64/libvendor.goodix.hardware.fingerprint@1.0.so
-cp -Raf $SVENDOR/lib64/com.fingerprints.extension@1.0.so $PVENDOR/lib64/com.fingerprints.extension@1.0.so
-cp -Raf $SVENDOR/lib64/libgf_ca.so $PVENDOR/lib64/libgf_ca.so
-cp -Raf $SVENDOR/lib64/libgf_hal.so $PVENDOR/lib64/libgf_hal.so
+cp -af $SVENDOR/app/FingerprintExtensionService/FingerprintExtensionService.apk $PVENDOR/app/FingerprintExtensionService/FingerprintExtensionService.apk
+cp -af $SVENDOR/framework/com.fingerprints.extension.jar $PVENDOR/framework/com.fingerprints.extension.jar
+cp -af $SVENDOR/lib64/hw/fingerprint.fpc.default.so $PVENDOR/lib64/hw/fingerprint.fpc.default.so
+cp -af $SVENDOR/lib64/hw/fingerprint.goodix.default.so $PVENDOR/lib64/hw/fingerprint.goodix.default.so
+cp -af $SVENDOR/lib64/vendor.qti.hardware.fingerprint@1.0.so $PVENDOR/lib64/vendor.qti.hardware.fingerprint@1.0.so
+cp -af $SVENDOR/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so $PVENDOR/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so
+cp -af $SVENDOR/lib64/libvendor.goodix.hardware.fingerprint@1.0.so $PVENDOR/lib64/libvendor.goodix.hardware.fingerprint@1.0.so
+cp -af $SVENDOR/lib64/com.fingerprints.extension@1.0.so $PVENDOR/lib64/com.fingerprints.extension@1.0.so
+cp -af $SVENDOR/lib64/libgf_ca.so $PVENDOR/lib64/libgf_ca.so
+cp -af $SVENDOR/lib64/libgf_hal.so $PVENDOR/lib64/libgf_hal.so
 
-cp -Raf $SSYSTEM/system/usr/keylayout/uinput-fpc.kl $PSYSTEM/system/usr/keylayout/uinput-fpc.kl
-cp -Raf $SSYSTEM/system/usr/idc/uinput-fpc.idc $PSYSTEM/system/usr/idc/uinput-fpc.idc
-cp -Raf $SSYSTEM/system/usr/keylayout/uinput-fpc.kl $PSYSTEM/system/usr/keylayout/uinput-fpc.kl
-cp -Raf $SSYSTEM/system/usr/idc/uinput-fpc.idc $PSYSTEM/system/usr/idc/uinput-fpc.idc
+cp -af $SSYSTEM/system/usr/keylayout/uinput-fpc.kl $PSYSTEM/system/usr/keylayout/uinput-fpc.kl
+cp -af $SSYSTEM/system/usr/idc/uinput-fpc.idc $PSYSTEM/system/usr/idc/uinput-fpc.idc
+cp -af $SSYSTEM/system/usr/keylayout/uinput-fpc.kl $PSYSTEM/system/usr/keylayout/uinput-fpc.kl
+cp -af $SSYSTEM/system/usr/idc/uinput-fpc.idc $PSYSTEM/system/usr/idc/uinput-fpc.idc
 
 #GOODSEX
 
@@ -215,12 +218,12 @@ rm -rf $PSYSTEM/system/etc/firmware
 cp -Raf $SSYSTEM/system/etc/firmware/* $PVENDOR/firmware/
 
 
-cp -Raf $OUTP/libwifi-hal64.so $PVENDOR/lib64/libwifi-hal.so
+cp -f $OUTP/libwifi-hal64.so $PVENDOR/lib64/libwifi-hal.so
 chmod 644 $PVENDOR/lib64/libwifi-hal.so
 chown -hR root:root $PVENDOR/lib64/libwifi-hal.so
 setfattr -h -n security.selinux -v u:object_r:vendor_file:s0 $PVENDOR/lib64/libwifi-hal.so
 
-cp -Raf $OUTP/libwifi-hal32.so $PVENDOR/lib/libwifi-hal.so
+cp -f $OUTP/libwifi-hal32.so $PVENDOR/lib/libwifi-hal.so
 chmod 644 $PVENDOR/lib/libwifi-hal.so
 chown -hR root:root $PVENDOR/lib/libwifi-hal.so
 setfattr -h -n security.selinux -v u:object_r:vendor_file:s0 $PVENDOR/lib/libwifi-hal.so
@@ -252,8 +255,8 @@ cp -Raf $SVENDOR/etc/acdbdata $PVENDOR/etc/acdbdata
 
 #statusbar/corner
 rm -rf $PVENDOR/app/NotchOverlay
-cp -Raf $FILES/overlay/DevicesOverlay.apk $PVENDOR/overlay/DevicesOverlay.apk
-cp -Raf $FILES/overlay/DevicesAndroidOverlay.apk $PVENDOR/overlay/DevicesAndroidOverlay.apk
+cp -f $FILES/overlay/DevicesOverlay.apk $PVENDOR/overlay/DevicesOverlay.apk
+cp -f $FILES/overlay/DevicesAndroidOverlay.apk $PVENDOR/overlay/DevicesAndroidOverlay.apk
 chmod 644 $PVENDOR/overlay/DevicesOverlay.apk
 chmod 644 $PVENDOR/overlay/DevicesAndroidOverlay.apk
 chown -hR root:root $PVENDOR/overlay/DevicesOverlay.apk
@@ -262,8 +265,8 @@ setfattr -h -n security.selinux -v u:object_r:vendor_overlay_file:s0 $PVENDOR/ov
 setfattr -h -n security.selinux -v u:object_r:vendor_overlay_file:s0 $PVENDOR/overlay/DevicesAndroidOverlay.apk
 
 #readingmode 
-cp -Raf $FILES/readingmode/qdcm_calib_data_jdi_nt36672_fhd_video_mode_dsi_panel.xml $PVENDOR/etc/qdcm_calib_data_jdi_nt36672_fhd_video_mode_dsi_panel.xml
-cp -Raf $FILES/readingmode/qdcm_calib_data_tianma_nt36672_fhd_video_mode_dsi_panel.xml $PVENDOR/etc/qdcm_calib_data_tianma_nt36672_fhd_video_mode_dsi_panel.xml
+cp -f $FILES/readingmode/qdcm_calib_data_jdi_nt36672_fhd_video_mode_dsi_panel.xml $PVENDOR/etc/qdcm_calib_data_jdi_nt36672_fhd_video_mode_dsi_panel.xml
+cp -f $FILES/readingmode/qdcm_calib_data_tianma_nt36672_fhd_video_mode_dsi_panel.xml $PVENDOR/etc/qdcm_calib_data_tianma_nt36672_fhd_video_mode_dsi_panel.xml
 chmod 644 $PVENDOR/etc/qdcm_calib_data_jdi_nt36672_fhd_video_mode_dsi_panel.xml
 chmod 644 $PVENDOR/etc/qdcm_calib_data_tianma_nt36672_fhd_video_mode_dsi_panel.xml
 chown -hR root:root $PVENDOR/etc/qdcm_calib_data_jdi_nt36672_fhd_video_mode_dsi_panel.xml
@@ -294,15 +297,26 @@ rmdir $PVENDOR
 rmdir $SSYSTEM
 rmdir $SVENDOR
 
-e2fsck -y -f systemn7.img
-resize2fs systemn7.img 786432
+e2fsck -y -f $OUTP/systemport.img
+resize2fs $OUTP/systemport.img 786432
 
-mv vendorn7.img $OUTP/zip/vendor_new.img
-mv systemn7.img $OUTP/zip/system_new.img
+
+img2simg $OUTP/systemport.img $OUTP/sparsesystem.img
+rm $OUTP/systemport.img
+$TOOLS/img2sdat/img2sdat.py -v 4 -o $OUTP/zip -p system $OUTP/sparsesystem.img
+rm $OUTP/sparsesystem.img
+img2simg $OUTP/vendorport.img $OUTP/sparsevendor.img
+rm $OUTP/vendorport.img
+$TOOLS/img2sdat/img2sdat.py -v 4 -o $OUTP/zip -p vendor $OUTP/sparsevendor.img
+rm $OUTP/sparsevendor.img
+brotli -j -v -q 6 $OUTP/zip/system.new.dat
+brotli -j -v -q 6 $OUTP/zip/vendor.new.dat
 
 cd $OUTP/zip
 zip -ry $OUTP/10_MIUI_11_jasmine_sprout_$ROMVERSION.zip *
 cd $CURRENTDIR
+rm -rf $OUTP/zip
+chown -hR $CURRENTUSER:$CURRENTUSER $OUTP
 
-rm systema2.img
-rm vendora2.img
+rm $OUTP/systema2.img
+rm $OUTP/vendora2.img
